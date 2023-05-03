3491204722848949e77fde65dcbb49d514b18d8c
Ghahremnaialireza <ghahremani.alireza1379@gmail.com>
2023-05-03 20:27:09 +0330

    add datatype example

---
+++ simple-python-datatype/python-datatypes.py
@@ -0,0 +1,20 @@
+## DATA TYPE
+# everything between "" are string in here
+print ("heyyy its string you can use '' too ")
+
+# integer (int)
+# whole numbers , positive or negatives and ... are int
+print (12)
+
+
+# calculation in python (simple app)
+# how many second are there in a year ???
+# use string Concatenation
+# str(somethinginhere) will change that type to string
+# print(f"....{50}....")  {}will not print but 50 will as str
+sec = 365 * 24 * 60 * 60
+print ("We have " + str(sec) + " seconds in a year" )
+print (f"We have {sec} seconds in a year")
+
+
+
