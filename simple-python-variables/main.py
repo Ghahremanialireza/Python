440d818db7617e50540fede12ca33b57869bc203
Ghahremnaialireza <ghahremani.alireza1379@gmail.com>
2023-05-03 20:27:16 +0330

    add variable description

---
+++ simple-python-variables/main.py
@@ -0,0 +1,17 @@
+
+### VARIABLES
+# variables are containers for storing values
+# instead of repeating a single string we can use variable
+# like this ...
+
+name = "Alireza Ghahremani Masoom Abad"
+# name is VAR and i assign my name to it
+print(f"My name is {name}")
+print(f"Is my name ({name}) too short ?? :)")
+
+# we can seprate words in single VARname with __ like m_yme
+# NOTE : there some keywords that they called reserved keywords
+# Gooooooogle it :-()
+
+
+# we can just change the variables value insted of change whole code
